915aa74dd82ec36667a3822ed4597bbaad706d88
Kjiwoo <asqw3735@gmail.com>
2026-06-02 16:53:58 +0900

    feat: implement customer review features

--- DB8/src/main/java/model/CustomerProfileHistory.java
+++ DB8/src/main/java/model/CustomerProfileHistory.java
@@ -1,4 +1,72 @@
 package model;
+import java.time.LocalDateTime;
 
 public class CustomerProfileHistory {
+    private int profileId;
+    private int customerId;
+    private String city;
+    private String membershipLevel;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public CustomerProfileHistory() {}
+
+    public CustomerProfileHistory(int profileId, int customerId, String city,
+                                  String membershipLevel, LocalDateTime startDate,
+                                  LocalDateTime endDate) {
+        this.profileId = profileId;
+        this.customerId = customerId;
+        this.city = city;
+        this.membershipLevel = membershipLevel;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public int getProfileId() {
+        return profileId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getMembershipLevel() {
+        return membershipLevel;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setMembershipLevel(String membershipLevel) {
+        this.membershipLevel = membershipLevel;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setProfileId(int profileId) {
+        this.profileId = profileId;
+    }
 }
--- DB8/src/main/java/service/CustomerService.java
+++ DB8/src/main/java/service/CustomerService.java
@@ -1,5 +1,137 @@
 package service;
 
+import dao.CustomerDAO;
+import dao.ReviewDAO;
+import util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+
 public class CustomerService {
-    //TODO: Handle customer-related business logic
+    //TODO: Handel customer-realted business logic
+    private final CustomerDAO customerDAO = new CustomerDAO();
+    private final ReviewDAO reviewDAO = new ReviewDAO();
+
+    // Register customer
+    public void registerCustomer(int customerId,
+                                 String firstName,
+                                 String lastName,
+                                 String email,
+                                 String phone,
+                                 Date birthDate) {
+
+        try (Connection conn = DBConnection.getConnection()) {
+
+            customerDAO.registerCustomer(
+                    conn,
+                    customerId,
+                    firstName,
+                    lastName,
+                    email,
+                    phone,
+                    birthDate
+            );
+
+            System.out.println("Customer registered.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Update customer profile (transaction)
+    public void updateCustomerProfile(int customerId,
+                                      String firstName,
+                                      String lastName,
+                                      String email,
+                                      String phone,
+                                      Date birthDate,
+                                      int newProfileId,
+                                      String city,
+                                      String membershipLevel) {
+
+        try (Connection conn = DBConnection.getConnection()) {
+
+            conn.setAutoCommit(false);
+
+            try {
+                customerDAO.updateCustomerProfile(
+                        conn,
+                        customerId,
+                        firstName,
+                        lastName,
+                        email,
+                        phone,
+                        birthDate
+                );
+
+                customerDAO.closeCurrentProfileHistory(conn, customerId);
+                customerDAO.insertCustomerProfileHistory(
+                        conn,
+                        newProfileId,
+                        customerId,
+                        city,
+                        membershipLevel
+                );
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                throw e;
+            } finally {
+                conn.setAutoCommit(true);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Write review
+    public void writeReview(int reviewId,
+                            int customerId,
+                            int productId,
+                            int rating,
+                            String reviewText) {
+
+        try (Connection conn = DBConnection.getConnection()) {
+            boolean purchased =
+                    reviewDAO.checkPurchaseBeforeReview(
+                            conn,
+                            customerId,
+                            productId
+                    );
+            if (!purchased) {
+                System.out.println("Customer did not purchase this book.");
+                return;
+            }
+            reviewDAO.writeReview(
+                    conn,
+                    reviewId,
+                    customerId,
+                    productId,
+                    rating,
+                    reviewText
+            );
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Delete review
+    public void deleteReview(int reviewId,
+                             int customerId) {
+        try (Connection conn = DBConnection.getConnection()) {
+
+            reviewDAO.deleteReview(
+                    conn,
+                    reviewId,
+                    customerId
+            );
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
